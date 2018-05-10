bc330fde481b40ba6f729a4543f2d3a5cafa22df
vinson <wxgsorry@gmail.com>
2018-05-10 17:58:02 +0800

    :beer: 删除首页地址

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 
-var cacheStorageKey = 'minimal-pwa-3';
+var cacheStorageKey = 'minimal-pwa-4';
 var cacheList = [
-  	'/',
   	'index.html',
   	'main.css',
   	'logo.png'
