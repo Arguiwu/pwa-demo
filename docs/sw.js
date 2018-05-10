2a223241173b34e794e35884e2c93890a406b12b
vinson <wxgsorry@gmail.com>
2018-05-10 17:44:18 +0800

    :rocket: initial commit

---
+++ docs/sw.js
@@ -0,0 +1,45 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+
+var cacheStorageKey = 'minimal-pwa-3';
+var cacheList = [
+  	'/',
+  	'index.html',
+  	'main.css',
+  	'logo.png'
+];
+
+self.addEventListener('install', e => {
+	e.waitUntil(
+		caches.open(cacheStorageKey)
+		.then(cache => cache.addAll(cacheList))
+		.then(() => self.skipWaiting())
+	);
+});
+
+self.addEventListener('fetch', e => {
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+			if(response != null){
+				return response
+			}
+			return fetch(e.request.url)
+		})
+	);
+});
+
+
+self.addEventListener('activate', e => {
+	e.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.filter(cacheNames => {
+					return cacheNames !== cacheStorageKey
+				}).map(cacheNames => {
+					return caches.delete(cacheNames)
+				})
+				)
+		}).then(() => {
+			return self.clients.claim()
+		})
+	);
+});
